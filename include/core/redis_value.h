968e0e24e18eab4c696aa2ccd6a3608b4212a3dd
Jiwon Park <qwes8873@gmail.com>
2025-08-16 21:31:58 +0900

    Expand RedisValue to support all Redis data types

--- include/core/redis_value.h
+++ include/core/redis_value.h
@@ -1,24 +1,38 @@
 #pragma once
 
 #include <string>
-#include <vector>
+#include <deque>
 #include <unordered_map>
 #include <unordered_set>
 #include <set>
 
 enum class RedisValueType {
     String,
+    List,
+    Hash,
+    Set,
+    SortedSet,
 };
 
 class RedisValue {
 public:
-    using StringValue = std::string;
+    using RedisString = std::string;
+    using RedisList = std::deque<RedisValue>;
+    using RedisHash = std::unordered_map<RedisString, RedisValue>;
+    using RedisSet = std::unordered_set<RedisString>;
+    using RedisSortedSet = std::set<RedisString>;
+    using RedisVariant = std::variant<RedisString, RedisList, RedisHash, RedisSet, RedisSortedSet>;
 
-    RedisValue(StringValue& value) : type_(RedisValueType::String), value_(value) {}
+    explicit RedisValue(RedisString& value) : type_(RedisValueType::String), value_(value) {}
+    explicit RedisValue(RedisList& value) : type_(RedisValueType::List), value_(value) {}
+    explicit RedisValue(RedisHash& value) : type_(RedisValueType::Hash), value_(value) {}
+    explicit RedisValue(RedisSet& value) : type_(RedisValueType::Set), value_(value) {}
+    explicit RedisValue(RedisSortedSet& value) : type_(RedisValueType::SortedSet), value_(value) {}
 
-    const StringValue& value() { return value_; }
+    const RedisVariant& value() const { return value_; }
+    RedisValueType type() const { return type_;}
 
 private:
     RedisValueType type_;
-    StringValue value_;
+    RedisVariant value_;
 };
